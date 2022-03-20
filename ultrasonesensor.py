ad03848d09e23f16e49c26e3d69d9a1d974ebf99
raspipy <100288938+raspipy@users.noreply.github.com>
2022-03-20 12:12:13 +0100

    Create ultrasonesensor.py

---
+++ ultrasonesensor.py
@@ -0,0 +1,35 @@
+import RPi.GPIO as GPIO
+import time
+class ultrasonesensor:
+    def __init__(self, pin):
+        self.pin = pin
+    def get_data(self):
+        # --- Setup --- 
+
+        GPIO.setup(self.pin, GPIO.OUT)
+        GPIO.output(self.pin, GPIO.LOW)
+        time.sleep(2/1000000)
+        GPIO.output(self.pin, GPIO.HIGH)
+        time.sleep(5/1000000)
+        GPIO.output(self.pin, GPIO.LOW)
+        # --- Measurement --- 
+        GPIO.setup(self.pin, GPIO.IN)
+        while GPIO.input(self.pin) != 1:
+            pass
+        begin_tijd= time.time_ns()
+        while GPIO.input(self.pin) != 0:
+            pass
+        eind_tijd= time.time_ns()
+        return ((begin_tijd - eind_tijd)/ 2 * 34 /1000000)
+
+
+
+
+
+
+
+
+        
+
+
+
