734a779cd808c6bc955952196677e3c8a4e15f47
raspipy <100288938+raspipy@users.noreply.github.com>
2022-04-06 14:52:02 +0200

    Update kleurensensor.py

--- classes/kleurensensor.py
+++ classes/kleurensensor.py
@@ -1,6 +1,6 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.set_mode(GPIO.BOARD)
+GPIO.set_mode(GPIO.BOARD) # Sets the GPIO mode to BOARD
 class kleurensensors():
     def __init__(self, out_pins, in_pins):
         self.out_pins = out_pins
@@ -11,18 +11,32 @@ class kleurensensors():
             GPIO.setup(i, GPIO.IN)
 
         GPIO.output(self.out_pins[0], GPIO.LOW)
+
         GPIO.output(self.out_pins[1], GPIO.HIGH)
+
         GPIO.output(self.out_pins[2], GPIO.HIGH)
+
         GPIO.output(self.out_pins[3], GPIO.HIGH)
+
     def get_data(self):
+        # --- Sensor 1 --- #
 
         GPIO.wait_for_edge(self.in_pins[0], GPIO.BOTH)
+
         begin_tijd = time.time_ns()
+
         GPIO.wait_for_edge(self.in_pins[0], GPIO.BOTH)
+
         tijdsduur1 = time.time_ns() - begin_tijd
+        # --- Sensor 2 --- #
+
         GPIO.wait_for_edge(self.in_pins[1], GPIO.BOTH)
+
         begin_tijd = time.time_ns()
         GPIO.wait_for_edge(self.in_pins[1], GPIO.BOTH)
+
         tijdsduur2 = time.time_ns() - begin_tijd
-        return (tijdsduur1, tijdsduur2)
+
+
+        return (tijdsduur1, tijdsduur2) # Return the result in a tuple
 
