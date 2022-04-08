ce2f93447e7631695b4ea991896845b9aac23c68
raspipy <100288938+raspipy@users.noreply.github.com>
2022-04-08 12:32:59 +0200

    Update

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import RPi.GPIO as GPIO
-
+import time
+import random
+from classes.ultrasonesensor import ultrasonesensor
 from classes.lijnsensor import lijnsensor
 from classes.motor import motor
 from classes.kleurensensor import kleurensensor
@@ -10,11 +12,14 @@ lijnsensor = lijnsensor([11, 13, 15, 19, 21, 23, 29, 31])
 print(lijnsensor.readData())
 
 motor1 = motor([35,37,32,8])
+ultrasonesensor = ultrasonesensor(16)
 kleurensensoren = kleurensensor([40, 38, 36, 26], [22, 18])
 
-print(kleurensensoren.get_data())
+print(kleurensensoren.get_data_full())
 
 while True:
-    motor1.drive(50)
+    print(ultrasonesensor.get_data())
+    time.sleep(0.1)
+    motor1.drive(random.randint(0, 100))
 
 GPIO.cleanup()
