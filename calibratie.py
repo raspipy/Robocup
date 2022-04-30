e1ac85babc30213edcc47baccbd336be58e4cf9c
raspipy <100288938+raspipy@users.noreply.github.com>
2022-04-30 14:22:29 +0200

    EEE

--- calibratie.py
+++ calibratie.py
@@ -1,7 +1,10 @@
 from classes.lijnsensor import lijnsensor
 import RPi.GPIO as GPIO
 
+
 GPIO.setwarnings(False)
 lijnsensor = lijnsensor([11, 13, 15, 19, 21, 23, 29, 31])
-
-print(lijnsensor.get_data())
+for i in range(5):
+    input("Zet op een ander plaats op het zwart")
+    print(lijnsensor.get_data())
+GPIO.cleanup()
