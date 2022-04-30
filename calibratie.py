e9762c59f1e42a505136a93c4feea69bd7ad33ea
raspipy <100288938+raspipy@users.noreply.github.com>
2022-04-30 14:25:47 +0200

    E

--- calibratie.py
+++ calibratie.py
@@ -4,7 +4,14 @@ import RPi.GPIO as GPIO
 
 GPIO.setwarnings(False)
 lijnsensor = lijnsensor([11, 13, 15, 19, 21, 23, 29, 31])
+data_raw = []
+data = 0
 for i in range(5):
     input("Zet op een ander plaats op het zwart")
-    print(lijnsensor.get_data())
+    data_raw.append(lijnsensor.get_data())
+for i in range(len(data_raw)):
+    data = data + data_raw[i]
+data = data / len(data_raw)
+
+
 GPIO.cleanup()
