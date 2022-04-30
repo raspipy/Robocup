eab24f599cfdcb7d725ac1f745f4ec0c6c93db7b
lars2800 <lars.van.goethem@telenet.be>
2022-04-30 15:09:13 +0200

    added open function

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
-import RPi.GPIO as GPIO
+#############################
+#     importing libarys     #
+#############################
 import time
-import random
 from classes.ultrasonesensor import ultrasonesensor
 from classes.lijnsensor import lijnsensor
 from classes.motor import motor
 from classes.kleurensensor import kleurensensor
+import RPi.GPIO as GPIO
 
-GPIO.setwarnings(False)
+#############################
+#     variables             #
+#############################
+FileLoc = "calibratie_waarden.txt"
 
 lijnsensor = lijnsensor([11, 13, 15, 19, 21, 23, 29, 31])
-print(lijnsensor.get_data())
-
+kleurensensoren = kleurensensor([40, 38, 36, 26], [22, 18])
 motor1 = motor([35,37,32,8])
 ultrasonesensor = ultrasonesensor(16)
-kleurensensoren = kleurensensor([40, 38, 36, 26], [22, 18])
-
-print(kleurensensoren.get_data_full())
-
-while True:
-    print(str(ultrasonesensor.get_data()) + " cm")  
-    time.sleep(0.4)
-    motor1.drive(random.randint(0, 100))
-
+GPIO.setwarnings(False)
 GPIO.cleanup()
+#############################
+#     Open and Read File    #
+#############################
 
+File = open(FileLoc,"r")
+FileData = File.readline(1)
+File.close
