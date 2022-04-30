6dbc7cd5ddec731df09938a14d1ac0693c7a033a
raspipy <100288938+raspipy@users.noreply.github.com>
2022-04-30 15:40:43 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #############################
-#     importing libarys     #
+#     importing libaries     #
 #############################
 import time
 from classes.ultrasonesensor import ultrasonesensor
@@ -11,7 +11,6 @@ import RPi.GPIO as GPIO
 #############################
 #     variables             #
 #############################
-FileLoc = "classes/calibratie_waarden.txt"
 
 lijnsensor = lijnsensor([11, 13, 15, 19, 21, 23, 29, 31])
 kleurensensoren = kleurensensor([40, 38, 36, 26], [22, 18])
@@ -19,10 +18,5 @@ motor1 = motor([35,37,32,8])
 ultrasonesensor = ultrasonesensor(16)
 GPIO.setwarnings(False)
 GPIO.cleanup()
-#############################
-#     Open and Read File    #
-#############################
 
-File = open(FileLoc,"r")
-FileData = File.readline(1)
-File.close
+print(lijnsensor.get_data())
