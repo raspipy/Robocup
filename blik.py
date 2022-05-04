f496b64072b3d07ef44006ba4c63698721152214
raspipy <100288938+raspipy@users.noreply.github.com>
2022-05-04 17:41:01 +0200

    e
    EEEEE

--- blik.py
+++ blik.py
@@ -1,12 +1,12 @@
-from ultrasonesensor import ultrasonesensor
-from lijnsensor import lijnsensor
-from motor import motor
+from classes.ultrasonesensor import ultrasonesensor
+from classes.lijnsensor import lijnsensor
+from classes.motor import motor
 import time
 motor1 = motor([10,12,33,8])
 motor2 = motor([35,37,32,8])
 ultrasonesensor = ultrasonesensor(16)
 while ultrasonesensor.get_data() <= 20.0:
-    motor1.drive(-20)
+    motor1.drive(-0)
     motor2.drive(20)
     print(ultrasonesensor.get_data())
     time.sleep(0.1)
