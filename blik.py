204fa9554ebd4ea0bfdb64a75c884bc50ae89996
raspipy <100288938+raspipy@users.noreply.github.com>
2022-05-04 17:51:04 +0200

    eEeeEEeEeEEEeeeeEEeEEEeEEE
    E the language of e's

--- blik.py
+++ blik.py
@@ -9,9 +9,9 @@ motor1 = motor([10,12,33,8])
 motor2 = motor([35,37,32,8])
 
 ultrasonesensor = ultrasonesensor(16)
-while ultrasonesensor.get_data() <= 20.0:
+"""while ultrasonesensor.get_data() <= 20.0:
     motor1.drive(-0)
-    motor2.drive(20)
-print ("Found!")
+    motor2.drive(20)"""
+print (ultrasonesensor.get_data())
 
 GPIO.cleanup()
