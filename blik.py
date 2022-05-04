4e6a6d3bcdb090c79349c6eb42424b0f79bf68dd
raspipy <100288938+raspipy@users.noreply.github.com>
2022-05-04 17:46:51 +0200

    Update blik.py

--- blik.py
+++ blik.py
@@ -1,11 +1,13 @@
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+
 from classes.ultrasonesensor import ultrasonesensor
 from classes.lijnsensor import lijnsensor
 from classes.motor import motor
-import RPi.GPIO as GPIO
 import time
 motor1 = motor([10,12,33,8])
 motor2 = motor([35,37,32,8])
-GPIO.setwarnings(False)
+
 ultrasonesensor = ultrasonesensor(16)
 while ultrasonesensor.get_data() <= 20.0:
     motor1.drive(-0)
