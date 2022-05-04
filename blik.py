0855457d916281796562439d306b56eb14435264
raspipy <100288938+raspipy@users.noreply.github.com>
2022-05-04 17:54:38 +0200

    EEE
    eEEeEEeEEeeEEEeEEE

--- blik.py
+++ blik.py
@@ -12,6 +12,6 @@ ultrasonesensor = ultrasonesensor(16)
 while ultrasonesensor.get_data() >= 20.0:
     motor1.drive(-0)
     motor2.drive(20)
-print (ultrasonesensor.get_data())
-
+motor1.drive(20)
+motor2.drive(20)
 GPIO.cleanup()
