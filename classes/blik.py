624b5d2fd536338c692ae6b4d297e9bce994944b
raspipy <100288938+raspipy@users.noreply.github.com>
2022-05-04 17:34:20 +0200

    Update blik.py

--- classes/blik.py
+++ classes/blik.py
@@ -1,6 +1,8 @@
 from classes.ultrasonesensor import ultrasonesensor
 from classes.lijnsensor import lijnsensor
 from classes.motor import motor
+import time
 
 while ultrasonesensor.get_data() <= 20.0:
     print(ultrasonesensor.get_data())
+    time.sleep(0.1)
