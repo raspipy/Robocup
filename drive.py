fb661bae386b5a9c82e60d1f6b1c9c300597e426
lars2800 <lars.van.goethem@telenet.be>
2022-05-06 19:14:09 +0200

    chanchede driviing

--- drive.py
+++ drive.py
@@ -31,11 +31,11 @@ while True:
         speedMotor1 = basespeed
         speedMotor2 = basespeed
     elif position > 0:
-        speedMotor1 = position
-        speedMotor2 = position + basespeed
-    else:
         speedMotor1 = position + basespeed
         speedMotor2 = position
+    else:
+        speedMotor1 = position
+        speedMotor2 = position + basespeed
 
     motor1.drive(speedMotor1)
     motor2.drive(speedMotor2)
