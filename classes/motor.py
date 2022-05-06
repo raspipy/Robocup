0dc84b65bd9c365c570eee6c95b1b8301f5c708c
raspipy <100288938+raspipy@users.noreply.github.com>
2022-05-06 17:54:24 +0200

    Update motor.py

--- classes/motor.py
+++ classes/motor.py
@@ -4,11 +4,12 @@ pi.setmode(pi.BOARD)
 
 class motor:
     
-    def __init__(self,pins):
+    def __init__(self,pins, minumun_speed):
         
         self.pin_vooruit = pins[0]
         self.pin_achteruit = pins[1]
         self.pin_pwm = pins[2]
+        self.minumun_speed = minumun_speed
         
         pin_toggle = pins[3]
         pi.setup(pin_toggle,pi.OUT)
@@ -29,5 +30,9 @@ class motor:
             pi.output(self.pin_vooruit,0)
             pi.output(self.pin_achteruit,1)
             speed *= -1
+        if speed > 0 and speed < self.minumun_speed:
+            speed = self.minumun_speed
+
         self.pwm.ChangeDutyCycle(speed)
+        
         
