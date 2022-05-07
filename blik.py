51907533de40ebf6f4f4dc5f6acd484bc86a092d
lars2800 <lars.van.goethem@telenet.be>
2022-05-07 14:59:49 +0200

    random rot at blik.py

--- blik.py
+++ blik.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
+import random
 GPIO.setwarnings(False)
 
 from classes.ultrasonesensor import ultrasonesensor
@@ -9,12 +10,12 @@ motor1 = motor([10,12,33,8])
 motor2 = motor([35,37,32,8])
 
 ultrasonesensor = ultrasonesensor(16)
-while ultrasonesensor.get_data() >= 20.0:
-    motor1.drive(-0)
-    motor2.drive(40)
-motor1.drive()
-motor2.drive(20)
-time.sleep(3)
-motor1.drive(0)
-motor2.drive(0)
-GPIO.cleanup()
+
+randomint = random.random(0,1)
+
+if (randomint == 1):
+    motor1.drive(10)
+    motor2.drive(-10)
+else:
+    motor1.drive(-10)
+    motor2.drive(10)
