335056f7c41ad5a508baaa12540b6b8cb494a4dc
lars2800 <lars.van.goethem@telenet.be>
2022-05-08 16:24:40 +0200

    chanched ext§(zetfgbn

--- calibratie_kleurensensors.py
+++ calibratie_kleurensensors.py
@@ -8,10 +8,10 @@ groen_filter = [[0,0],[0,0],[0,0]]
 rood_filter = [[0,0],[0,0],[0,0]]
 clear_filter = [[0,0],[0,0],[0,0]]
 blue_filter = [[0,0],[0,0],[0,0]]
-verschil_groen_filter = [[0,0],[0,0]]
-verschil_rood_filter = [[0,0],[0,0]]
-verschil_clear_filter = [[0,0],[0,0]]
-verschil_blue_filter = [[0,0],[0,0]]
+verschil_groen_filter = [[0,0],[0,0],[0,0]]
+verschil_rood_filter = [[0,0],[0,0],[0,0]]
+verschil_clear_filter = [[0,0],[0,0],[0,0]]
+verschil_blue_filter = [[0,0],[0,0],[0,0]]
 possible_results = ["Red","Green","Blue","Clear"]
 texts = ["plaats de kleurensensors op het zwart","plaats de kleurensensors op het groen","plaats de kleurensensors op het geel"]
 for i in range(0,3):
@@ -43,17 +43,21 @@ for i in range(0,3):
         clear_filter[i][0] /= 25
         clear_filter[i][1] /= 25
 
-verschil_groen_filter[0] = min(groen_filter[1][0] - groen_filter[0][0], groen_filter[1][1] - groen_filter[0][1])
-verschil_groen_filter[1] = min(groen_filter[2][0] - groen_filter[1][0], groen_filter[2][1] - groen_filter[1][1])
+verschil_groen_filter[0] = min(abs(groen_filter[1][0] - groen_filter[0][0]), abs(groen_filter[1][1] - groen_filter[0][1]))
+verschil_groen_filter[1] = min(abs(groen_filter[2][0] - groen_filter[1][0]), abs(groen_filter[2][1] - groen_filter[1][1]))
+verschil_groen_filter[2] = min(abs(groen_filter[2][0] - groen_filter[0][0]), abs(groen_filter[2][1] - groen_filter[0][1]))
 
-verschil_rood_filter[0] = min(rood_filter[1][0] - rood_filter[0][0], rood_filter[1][1] - rood_filter[0][1])
-verschil_rood_filter[1] = min(rood_filter[2][0] - rood_filter[1][0], rood_filter[2][1] - rood_filter[1][1])
+verschil_rood_filter[0] = min(abs(rood_filter[1][0] - rood_filter[0][0]), abs(rood_filter[1][1] - rood_filter[0][1]))
+verschil_rood_filter[1] = min(abs(rood_filter[2][0] - rood_filter[1][0]), abs(rood_filter[2][1] - rood_filter[1][1]))
+verschil_rood_filter[2] = min(abs(rood_filter[2][0] - rood_filter[0][0]), abs(rood_filter[2][1] - rood_filter[0][1]))
 
-verschil_blue_filter[0] = min(blue_filter[1][0] - blue_filter[0][0], blue_filter[1][1] - blue_filter[0][1])
-verschil_blue_filter[1] = min(blue_filter[2][0] - blue_filter[1][0], blue_filter[2][1] - blue_filter[1][1])
+verschil_blue_filter[0] = min(abs(blue_filter[1][0] - blue_filter[0][0]), abs(blue_filter[1][1] - blue_filter[0][1]))
+verschil_blue_filter[1] = min(abs(blue_filter[2][0] - blue_filter[1][0]), abs(blue_filter[2][1] - blue_filter[1][1]))
+verschil_blue_filter[2] = min(abs(blue_filter[2][0] - blue_filter[0][0]), abs(blue_filter[2][1] - blue_filter[0][1]))
 
-verschil_clear_filter[0] = min(clear_filter[1][0] - clear_filter[0][0], clear_filter[1][1] - clear_filter[0][1])
-verschil_clear_filter[1] = min(clear_filter[2][0] - clear_filter[1][0], clear_filter[2][1] - clear_filter[1][1])
+verschil_clear_filter[0] = min(abs(clear_filter[1][0] - clear_filter[0][0]), abs(clear_filter[1][1] - clear_filter[0][1]))
+verschil_clear_filter[1] = min(abs(clear_filter[2][0] - clear_filter[1][0]), abs(clear_filter[2][1] - clear_filter[1][1]))
+verschil_clear_filter[2] = min(abs(clear_filter[2][0] - clear_filter[0][0]), abs(clear_filter[2][1] - clear_filter[0][1]))
 
 minimale_verschillen = [
     min(verschil_rood_filter),
