ccbb5d0fe6ac878851b37b1a51b9292891e3a711
raspipy <100288938+raspipy@users.noreply.github.com>
2022-05-11 18:56:47 +0200

    Update test.py

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ import time
 kleurensensor = kleurensensor([40, 38, 36, 26], [22, 18])
 
 while True:
-    print(kleurensensor.get_data())
+    print(str(kleurensensor.get_data()) + '\t' + str(kleurensensor.get_data_raw()))
     time.sleep(0.25)
