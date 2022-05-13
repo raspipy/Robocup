da1132dbba3f3b08c2336c52fced7091770a8ab7
raspipy <100288938+raspipy@users.noreply.github.com>
2022-05-13 19:10:00 +0200

    Update blik.py

--- blik.py
+++ blik.py
@@ -14,26 +14,20 @@ def find_blik(distance, speed, motor1, motor2):
     motor1.drive(0)
     motor2.drive(0)
     if (randomint == 1):
-        #motor1.drive(10)
-        #if ultrasonesensor.get_data() <= distance:
-        print(ultrasonesensor.get_data())
-
         while ultrasonesensor.get_data() >= distance:
             motor1.drive(-20)
-            print(ultrasonesensor.get_data())
             motor2.drive(20)
+        motor1.drive(-20)
+        motor2.drive(0)
+        time.sleep(0.1)
         motor1.drive(0)
         motor2.drive(0)
-
-    
-        #motor2.drive(-10)
     else:
-       # motor1.drive(-10)
-        #if ultrasonesensor.get_data() <= distance:
-        print(ultrasonesensor.get_data())
         while ultrasonesensor.get_data() >= distance:
             motor1.drive(20)
-            print(ultrasonesensor.get_data())
             motor2.drive(-20)
+        motor1.drive(0)
+        motor2.drive(-20)
+        time.sleep(0.1)
         motor1.drive(0)
         motor2.drive(0)
